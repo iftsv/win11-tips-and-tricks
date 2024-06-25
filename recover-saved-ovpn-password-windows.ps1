c0e8a5b16018b5324de7856c0fcf5e1f9e8548da
iftsv <27852432+iftsv@users.noreply.github.com>
2024-06-25 12:18:36 +0700

    Add files via upload

---
+++ recover-saved-ovpn-password-windows.ps1
@@ -0,0 +1,19 @@
+$keys = Get-ChildItem "HKCU:\Software\OpenVPN-GUI\configs"
+$items = $keys | ForEach-Object {Get-ItemProperty $_.PsPath}
+
+foreach ($item in $items)
+{
+	$encryptedbytes=$item.'key-data'
+	$entropy=$item.'entropy'
+	$entropy=$entropy[0..(($entropy.Length)-2)]
+	
+	if ($encryptedbytes) {
+		$decryptedbytes = [System.Security.Cryptography.ProtectedData]::Unprotect(
+			$encryptedBytes, 
+			$entropy, 
+			[System.Security.Cryptography.DataProtectionScope]::CurrentUser
+		)
+ 
+		Write-Host ($item.'PSChildName' + " -- " + [System.Text.Encoding]::Unicode.GetString($decryptedbytes))
+	}
+}
